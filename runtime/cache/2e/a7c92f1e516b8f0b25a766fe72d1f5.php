9a9e53f7bd6d83f180355c982a62ae07f538ec53
郭先森 <1207894100@qq.com>
2020-08-01 00:25:41 +0800

    缓存

--- runtime/cache/2e/a7c92f1e516b8f0b25a766fe72d1f5.php
+++ runtime/cache/2e/a7c92f1e516b8f0b25a766fe72d1f5.php
@@ -1,4 +1,4 @@
 <?php
 //000000000000
  exit();?>
-a:9:{s:2:"id";s:1:"2";s:10:"model_name";s:17:"[单]单一内容";s:12:"model_fields";s:3:"1,9";s:7:"model_c";s:4:"News";s:7:"model_a";s:6:"Single";s:8:"disorder";s:1:"0";s:6:"status";s:1:"1";s:6:"dosave";s:0:"";s:5:"ispic";i:0;}
+a:8:{s:2:"id";i:2;s:10:"model_name";s:17:"[单]单一内容";s:12:"model_fields";s:3:"1,9";s:7:"model_c";s:4:"News";s:7:"model_a";s:6:"Single";s:5:"ispic";i:0;s:8:"disorder";i:0;s:6:"status";i:1;}
