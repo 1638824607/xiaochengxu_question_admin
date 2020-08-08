20f71406d9e29d4f5442dc136251f23b82edfde5
郭先森 <1207894100@qq.com>
2020-08-09 01:13:40 +0800

    增加分页类

--- application/admin/config.php
+++ application/admin/config.php
@@ -8,6 +8,14 @@ return [
          '__ADMIN__'    => '/public/static/admin',
      ],
 
+    'paginate'               => [
 
+        'type'      => 'page\Page',//分页类
+
+        'var_page'  => 'page',
+
+        'list_rows' => 15,
+
+    ],
 
 ];
