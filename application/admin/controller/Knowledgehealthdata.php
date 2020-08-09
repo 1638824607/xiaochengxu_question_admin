0c33a4ab830d101929a814bd872dfacb3f32294c
shenruxiang <shenruxiang@lixiang.com>
2020-08-09 15:11:00 +0800

    测评数据统计

---
+++ application/admin/controller/Knowledgehealthdata.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 吾道建站
+ * Date: 20/7/6
+ * Time: 00:02
+ * (吾道建站)保留在任何时刻，对代码内容进行添加、修改的权利
+ * (吾道建站)对本代码可能出现的内容错误及缺失不承担任何责任。对此进行的更正将合并到此代码的最新版本中
+ * 本代码的最终解释权归属于(吾道建站)
+ * 版权所有：吾道建站 @2020
+ * 吾道建站合作联系方式 微信:18226849637   QQ:730249592  备注: 吾道建站
+ * 吾道建站座右铭:客户即合理 存在即合理
+ */
+
+namespace app\admin\controller;
+
+use app\admin\common\Purview;
+use app\admin\model\KnowledgeHealthQuestionModel;
+use app\admin\model\KnowledgeHealthRecordModel;
+use app\admin\model\KnowledgeMatchModel;
+use app\admin\model\KnowledgeMatchQuestionModel;
+use app\admin\model\KnowledgeMatchRecordModel;
+use think\Db;
+
+class Knowledgehealthdata extends Purview
+{
+    public function index()
+    {
+        $list = Db::table('tp_knowledge_data')->where('knowledge_type', 'health')->order('create_day desc')->paginate(20);
+
+        $this->assign('list', $list);
+        return $this->fetch();
+    }
+}
--- runtime/cache/f6/8ab6f69bfc11c544cddeab08957369.php
+++ runtime/cache/f6/8ab6f69bfc11c544cddeab08957369.php
@@ -1,4 +1,4 @@
 <?php
 //000000000000
  exit();?>
-a:12:{s:2:"id";s:1:"0";s:3:"pid";s:2:"41";s:7:"catname";s:12:"测评记录";s:7:"templet";s:20:"knowledgematchrecord";s:6:"sort_c";s:20:"Knowledgematchrecord";s:6:"sort_a";s:5:"index";s:6:"isopen";s:1:"1";s:8:"disorder";s:3:"240";s:6:"status";s:1:"1";s:6:"dosave";s:0:"";s:8:"model_id";s:0:"";s:8:"sendtime";i:1596952198;}
+a:12:{s:2:"id";s:1:"0";s:3:"pid";s:2:"49";s:7:"catname";s:19:"knowledgehealthdata";s:7:"templet";s:19:"knowledgehealthdata";s:6:"sort_c";s:19:"Knowledgehealthdata";s:6:"sort_a";s:5:"index";s:6:"isopen";s:1:"0";s:8:"disorder";s:3:"250";s:6:"status";s:1:"1";s:6:"dosave";s:0:"";s:8:"model_id";s:0:"";s:8:"sendtime";i:1596956648;}
