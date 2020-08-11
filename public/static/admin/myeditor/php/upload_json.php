fa52cecd89b12f9fa166b372ac8888b99100134d
shenruxiang <shenruxiang@lixiang.com>
2020-08-11 22:57:15 +0800

    修改优化

--- public/static/admin/myeditor/php/upload_json.php
+++ public/static/admin/myeditor/php/upload_json.php
@@ -21,7 +21,7 @@ $ext_arr = array(
 	'image' => array('gif', 'jpg', 'jpeg', 'png', 'bmp'),
 	'flash' => array('swf', 'flv', 'mp4'),
 	'media' => array('swf', 'flv', 'mp3', 'wav', 'wma', 'wmv', 'mid', 'avi', 'mpg', 'asf', 'rm', 'rmvb'),
-	'file' => array('doc', 'docx', 'xls', 'xlsx', 'ppt', 'htm', 'html', 'txt', 'zip', 'rar', 'gz', 'bz2', 'pdf', 'flv', 'mp4'),
+	'file' => array('doc', 'docx', 'xls', 'xlsx', 'ppt', 'htm', 'html', 'txt', 'zip', 'rar', 'gz', 'bz2', 'pdf', 'flv', 'mp4', 'mp3'),
 );
 //最大文件大小
 $max_size = 100000000000000000000000000000000;
